420fd41fee53d063fa3ec4dbd552e345b390d3d8
samima55 <54752074+samima55@users.noreply.github.com>
2020-11-15 23:11:34 +0100

    Add files via upload

---
+++ phploversblog/index.php
@@ -0,0 +1,30 @@
+<?php include 'includes/header.php' ?>
+<?php
+$db= new Database();
+//create query
+$query= "select*from posts ORDER BY id  DESC";
+//run query
+$posts=$db->select($query);
+
+//create query
+$query= "select*from categories";
+//run query
+$categories=$db->select($query);
+
+?>
+<?php if($posts) : ?>
+<?php while($row=$posts->fetch_assoc()) : ?>
+    <div class="blog-post">
+      <h2 class="blog-post-title"><?php echo $row['title']; ?></h2>
+      <p class="blog-post-meta"><?php echo formatDate($row['date']); ?> by <a href="#"><?php echo $row['author']; ?></a></p>
+        <?php echo shortenText($row['body']); ?>
+        <a class="readmore" href="post.php?id=<?php echo urlencode($row['id']); ?>">Read More</a>
+    </div><!-- /.blog-post -->
+
+  <?php endwhile; ?>
+
+<?php else : ?>
+  <p>There are no post yet</p>
+<?php endif; ?>
+
+    <?php include 'includes/footer.php' ?>
